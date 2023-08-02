d3ba7500074d0eb855a7dc80b1bffe531d2c45f5
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-08-02 13:44:50 +0200

    we add charging state

---
+++ src/app/service/tesla-ChargeState.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TeslaChargeStateService {
+  private apiUrl =
+    'https://localhost:7147/TeslaVehicleData/charge-state?saveInDataBase=false';
+
+  constructor(private http: HttpClient) {}
+
+  getRealTimeData(): Observable<any> {
+    return this.http.post<any>(this.apiUrl, null);
+  }
+}
