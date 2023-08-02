5609c27d98c3fef572230ddfa429472b1e9cb9e1
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-08-02 09:53:51 +0200

    we added climate state variables

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -10,6 +10,7 @@ declare const google: any;
   template: `
     <div id="map" style="width: 100%; height: 400px;"></div>
     <div *ngIf="realTimeDriveStateData">
+      <h3>DRIVE CONDITIONS</h3>
       <p>
         ActiveRouteLatitude: {{ realTimeDriveStateData.activeRouteLatitude }}
       </p>
@@ -32,7 +33,158 @@ declare const google: any;
       <p>Speed: {{ realTimeDriveStateData.speed }}</p>
       <p>Timestamp: {{ realTimeDriveStateData.timestamp }}</p>
       <p>Timestamp: {{ formattedTimestamp }}</p>
-      <p>ClimateState: {{ realTimeClimateStateData.insideTemperature }}</p>
+      <h3>CLIMATE CONDITIONS</h3>
+      <p>
+        AllowCabinOverheatProtection:
+        {{ realTimeClimateStateData.allowCabinOverheatProtection }}
+      </p>
+      <p>
+        AutoSeatClimateLeft:
+        {{ realTimeClimateStateData.autoSeatClimateLeft }}
+      </p>
+      <p>
+        AutoSeatClimateRight:
+        {{ realTimeClimateStateData.autoSeatClimateRight }}
+      </p>
+      <p>
+        BatteryHeater:
+        {{ realTimeClimateStateData.batteryHeater }}
+      </p>
+      <p>
+        BatteryHeaterNoPower:
+        {{ realTimeClimateStateData.batteryHeaterNoPower }}
+      </p>
+      <p>
+        BioweaponMode:
+        {{ realTimeClimateStateData.bioweaponMode }}
+      </p>
+      <p>
+        CabinOverheatProtection:
+        {{ realTimeClimateStateData.cabinOverheatProtection }}
+      </p>
+      <p>
+        CabinOverheatProtectionActivelyCooling:
+        {{ realTimeClimateStateData.CabinOverheatProtectionActivelyCooling }}
+      </p>
+      <p>
+        ClimateKeeperMode:
+        {{ realTimeClimateStateData.climateKeeperMode }}
+      </p>
+      <p>
+        CabinOverheatProtectionActivationTemperature:
+        {{
+          realTimeClimateStateData.cabinOverheatProtectionActivationTemperature
+        }}
+      </p>
+      <p>
+        DefrostMode:
+        {{ realTimeClimateStateData.defrostMode }}
+      </p>
+      <p>
+        DriverTemperatureSetting:
+        {{ realTimeClimateStateData.driverTemperatureSetting }}
+      </p>
+      <p>
+        FanStatus:
+        {{ realTimeClimateStateData.fanStatus }}
+      </p>
+      <p>
+        HVAC_AutoRequest:
+        {{ realTimeClimateStateData.hvacAutoRequest }}
+      </p>
+      <p>
+        Inside_Temperature:
+        {{ realTimeClimateStateData.insideTemperature }}
+      </p>
+      <p>
+        Is_AutoConditioning_On:
+        {{ realTimeClimateStateData.isAutoConditioningOn }}
+      </p>
+      <p>
+        Is_Climate_On:
+        {{ realTimeClimateStateData.isClimateOn }}
+      </p>
+      <p>
+        Is_Front_Defroster_On:
+        {{ realTimeClimateStateData.isFrontDefrosterOn }}
+      </p>
+      <p>
+        Is_Preconditioning:
+        {{ realTimeClimateStateData.isPreconditioning }}
+      </p>
+      <p>
+        Is_RearDefroster_On:
+        {{ realTimeClimateStateData.isRearDefrosterOn }}
+      </p>
+      <p>
+        Left_Temperature_Direction:
+        {{ realTimeClimateStateData.leftTemperatureDirection }}
+      </p>
+      <p>
+        Maximum_Available_Temperature:
+        {{ realTimeClimateStateData.maximumAvailableTemperature }}
+      </p>
+      <p>
+        Minimum_Available_Temperature:
+        {{ realTimeClimateStateData.minimumAvailableTemperature }}
+      </p>
+      <p>
+        Outside_Temperature:
+        {{ realTimeClimateStateData.outsideTemperature }}
+      </p>
+      <p>
+        Passenger_Temp_Setting:
+        {{ realTimeClimateStateData.passengerTempSetting }}
+      </p>
+      <p>
+        Remote_Heater_Control_Enabled:
+        {{ realTimeClimateStateData.remoteHeaterControlEnabled }}
+      </p>
+      <p>
+        right_Temp_Direction:
+        {{ realTimeClimateStateData.rightTempDirection }}
+      </p>
+      <p>
+        Seat_Heater_Left:
+        {{ realTimeClimateStateData.seatHeaterLeft }}
+      </p>
+      <p>
+        Seat_Heater_Rear_Center:
+        {{ realTimeClimateStateData.seatHeaterRearCenter }}
+      </p>
+      <p>
+        Seat_Heater_Rear_Left:
+        {{ realTimeClimateStateData.seatHeaterRearLeft }}
+      </p>
+      <p>
+        Seat_Heater_Rear_Right:
+        {{ realTimeClimateStateData.seatHeaterRearRight }}
+      </p>
+      <p>
+        Seat_Heater_Right:
+        {{ realTimeClimateStateData.seatHeaterRight }}
+      </p>
+      <p>
+        Side_Mirror_Heaters:
+        {{ realTimeClimateStateData.sideMirrorHeaters }}
+      </p>
+      <p>
+        Steering_Wheel_Heater:
+        {{ realTimeClimateStateData.steeringWheelHeater }}
+      </p>
+      <p>
+        Supports_Fan_Only_Cabin_Overheat_Protection:
+        {{ realTimeClimateStateData.supportsFanOnlyCabinOverheatProtection }}
+      </p>
+      <p>
+        TimeStamp:
+        {{ realTimeClimateStateData.timestamp }}
+      </p>
+      <p>Timestamp: {{ formattedTimestamp }}</p>
+      <p>
+        Wiper_Blade_Heater:
+        {{ realTimeClimateStateData.wiperBladeHeater }}
+      </p>
     </div>
   `,
   styles: [
