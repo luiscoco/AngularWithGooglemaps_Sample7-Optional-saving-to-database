9d3fc42b4fe1038876613c1c728c177d61b01e80
Luis Coco Enríquez <LEnriquez@luxoft.com>
2023-08-03 12:19:45 +0200

    we added the Tesla official login page

--- src/app/components/get-login-url/get-login-url.component.ts
+++ src/app/components/get-login-url/get-login-url.component.ts
@@ -20,12 +20,21 @@ export class LoginComponent implements OnInit {
   getLoginUrl() {
     this.teslaLoginService.getLoginUrl().subscribe(
       (data) => {
-        this.loginUrl = data.login_url; // Assuming the response from the API contains a property "url" with the login URL.
-        this.codeVerifier = data.codeVerifier; // Assuming the response from the API contains a property "url" with the login URL.
+        this.loginUrl = data.login_url; // Assuming the response from the API contains a property "login_url" with the login URL.
+        this.codeVerifier = data.codeVerifier; // Assuming the response from the API contains a property "codeVerifier" with the code verifier.
+        this.openLoginUrlInNewTab(); // Redirect to the login URL in a new tab after obtaining it from the service.
       },
       (error) => {
         console.error('Error fetching login URL:', error);
       }
     );
+  }
+
+  openLoginUrlInNewTab() {
+    // Check if the login URL is available
+    if (this.loginUrl) {
+      // Open the login URL in a new tab
+      window.open(this.loginUrl, '_blank');
+    }
   }
 }
